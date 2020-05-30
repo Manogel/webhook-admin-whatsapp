6ab312cdc4847420decd2b7ac68d5055c3c5d797
Manogel <manoelgomes53@gmail.com>
2020-05-30 15:19:49 -0300

    #892 - adjustments on socket

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,23 @@
 import "dotenv/config";
-import express from "express";
+import express, { request } from "express";
 import axios from "axios";
 import socketio from "socket.io";
 import http from "http";
-// import socketWebhook from "./socketWebhook";
-
-import webhookConfig from "./config/webhook";
 import ProcessDataService from "./services/ProcessData.service";
 
 const app = express();
-const server = http.createServer(app);
-
-const io = socketio(server);
 
 app.use(express.json({ limit: "100000kb" }));
 app.use(express.urlencoded({ extended: true, limit: "100000kb" }));
 
+// Estraindo o protocolo utilizado pelo express
+const server = http.createServer(app);
+
+const io = socketio(server);
+
 io.on("connection", (socket) => {
-  console.log(`Socket conectado ${socket.id}`);
+  console.log(`${socket.handshake.query.user || socket.id} conectado.`);
+
   socket.on("sendMsg", (data) => {
     console.log("Enviando mensagem recebida...");
     socket.broadcast.emit("receivedMsg", data);
@@ -35,11 +35,6 @@ app.post("/webhook", async (request, response) => {
   return response.send("ok");
 });
 
-// socketWebhook.on("receivedMsg", async (data: any) => {
-//   await ProcessDataService.execute(data);
-//   await axios.post(webhookConfig.api_mchat_url, { webhook_data: data });
-// });
-
-app.listen(process.env.PORT || 3000, () => {
+server.listen(process.env.PORT || 3000, () => {
   console.log(`🚀 Server running on port ${process.env.PORT || 3000}`);
 });
